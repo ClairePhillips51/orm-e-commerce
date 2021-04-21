824f51932ca8ec88f611bee60da9d84e152cdfbb
Claire Phillips <claireacam@gmail.com>
2021-04-21 16:19:19 -0400

    completed category routes and added product routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,9 +5,11 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products (typo?)
+  // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{model: Product}],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -19,7 +21,7 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      // JOIN with Products, using the Trip through table
+      // JOIN with Product
       include: [{ model: Product}]
     });
 
